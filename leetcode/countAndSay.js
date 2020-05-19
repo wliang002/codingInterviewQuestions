b0a2e429d85dfb2f5e0397e1e58bb2b78af2ac4d
wliang002 <wliang002@berkeley.edu>
2020-05-18 23:27:25 -0700

    38

---
+++ leetcode/countAndSay.js
@@ -0,0 +1,14 @@
+// 38
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function(n) {
+    if(n === 1) {
+        return '1'
+    }
+};
+
+
+let n = 5
+console.log(countAndSay(n))
